9cce635b05f855fe9a7dbc2c8e4c3e102d0058c3
Alex Virilli <alexvirilli@gmail.com>
2025-02-27 12:16:33 -0500

    added drop down service menu

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './Header.css';
 import brand_logo from "./images/new_logo.png";
-import { Helmet } from 'react-helmet';
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleDropdown = () => {
+    if (isDropdownOpen) {
+      navigate('/services'); // Redirect to /services if dropdown is already open
+      setIsDropdownOpen(false);
+    } else {
+      setIsDropdownOpen(true);
+    }
+  };
+
   const closeMenu = () => {
     setIsOpen(false);
+    setIsDropdownOpen(false);
   };
 
   return (
@@ -25,7 +36,23 @@ function Header() {
       </button>
       <nav className={`nav-links ${isOpen ? 'open' : ''}`}>
         <NavLink exact to="/" activeClassName="active" onClick={closeMenu}>Home</NavLink>
-        <NavLink to="/services" activeClassName="active" onClick={closeMenu}>Services</NavLink>
+
+        {/* Dropdown Menu for Services */}
+        <div className="dropdown">
+          <button className="dropbtn" onClick={toggleDropdown}>
+            Services ▼
+          </button>
+          <div className={`dropdown-content ${isDropdownOpen ? 'show' : ''}`}>
+            <NavLink to="/services/Powerwashing" activeClassName="active" onClick={closeMenu}>Powerwashing</NavLink>
+            <NavLink to="/services/HouseWashing" activeClassName="active" onClick={closeMenu}>House Washing</NavLink>
+            <NavLink to="/services/WindowCleaning" activeClassName="active" onClick={closeMenu}>Window Cleaning</NavLink>
+            <NavLink to="/services/BinCleaning" activeClassName="active" onClick={closeMenu}>Bin Cleaning</NavLink>
+            <NavLink to="/services/RoofCleaning" activeClassName="active" onClick={closeMenu}>Roof Washing</NavLink>
+            <NavLink to="/services/GutterCleaning" activeClassName="active" onClick={closeMenu}>Gutter Cleaning</NavLink>
+            <NavLink to="/services/CommercialPowerwashing" activeClassName="active" onClick={closeMenu}>Commercial</NavLink>
+          </div>
+        </div>
+
         <NavLink to="/contact" activeClassName="active" onClick={closeMenu}>Contact</NavLink>
       </nav>
     </header>
