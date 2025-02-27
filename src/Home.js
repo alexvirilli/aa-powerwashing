2d5ce3bee1f1052e7e6dde50193715d14b3441f6
Alex Virilli <alexvirilli@gmail.com>
2025-02-27 17:53:06 -0500

    cleaned up and converted many files to webp

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import ImageComparison from './ImageComparison';
-import CustomerReviews from './CustomerReviews';
-import AccordionItem from './AccordionItem';
-import before1 from './images/before1.jpeg';
-import after1 from './images/after1.jpeg';
-import before2 from './images/before2.jpeg';
-import after2 from './images/after2.jpeg'
-import before3 from './images/before3.jpeg';
-import after3 from './images/after3.jpeg'
+
 import before4 from './images/before4.jpeg';
 import after4 from './images/after4.jpeg';
-import before5 from './images/before5.jpeg';
-import after5 from './images/after5.jpeg';
 // import image1 from './images/home1.jpeg';
 // import image2 from './images/home2.jpeg';
 // import image3 from './images/home3.jpeg';
@@ -24,12 +15,12 @@ import after5 from './images/after5.jpeg';
 // import image8 from './images/home8.jpeg';
 import image9 from './images/home9.webp';
 // import image10 from './images/home10.jpeg';
-import paver1 from './images/paver1.jpeg';
-import paver2 from './images/paver2.jpeg';
-import wall1 from './images/wall1.jpeg';
-import wall2 from './images/wall2.jpeg';
-import roof1 from './images/roof1.jpeg';
-import roof2 from './images/roof2.jpeg';
+import paver1 from './images/paver1.webp';
+import paver2 from './images/paver2.webp';
+import wall1 from './images/wall1.webp';
+import wall2 from './images/wall2.webp';
+import roof1 from './images/roof1.webp';
+import roof2 from './images/roof2.webp';
 
 import { Helmet } from 'react-helmet';
 
@@ -212,25 +203,6 @@ function Home() {
                     {/* <CustomerReviews /> */}
                 </div>
             </section>
-
-            {/* <section className="faq">
-                <h2>Frequently Asked Questions</h2>
-                <AccordionItem title="How much do your powerwashing services cost?">
-                    Our pricing is competitive and based on the specific needs of each job. We offer free estimates, so please contact us to get a customized quote for your project.
-                </AccordionItem>
-                <AccordionItem title="How do I schedule a powerwashing service?">
-                    You can schedule a service by calling us at (856) 281-2658, emailing us at aapowerwashers@gmail.com, or filling out the contact form on our website. We will get back to you promptly to arrange an appointment.
-                </AccordionItem>
-                <AccordionItem title="What areas do you serve?">
-                    We serve New Jersey, primarily New Brunswick, Cherry Hill, and the surrounding regions. If you are unsure whether we service your area, please contact us, and we’ll be happy to assist.
-                </AccordionItem>
-                <AccordionItem title='Why should I choose A&A Powerwashing?'>
-                    As hardworking college students, we bring dedication, enthusiasm, and a strong work ethic to every job. We offer high-quality, affordable powerwashing services with a focus on customer satisfaction and environmental responsibility.
-                </AccordionItem>
-            </section> */}
-
-            
-
 
             <footer className="business-footer">
                 <div className="footer-section contact-info">
